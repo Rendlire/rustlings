e2f7ebee721594213b8882de8d7e848aaf1c75a8
Rendlire <rendlire@gmail.com>
2021-07-05 12:50:07 +0900

    add

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -16,7 +16,7 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("weightless package")
         } else {
             return Package {
                 sender_country,
